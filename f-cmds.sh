0def7dcb8317fda725b780719768290baa399303
Vishnu V <vishnukpn884@gmail.com>
2021-02-18 17:17:01 +0700

    V0.1

--- f-cmds.sh
+++ f-cmds.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-
 fcmd(){
     alias b="cd .."
+    alias myip="ifconfig | grep broadcast | awk '{print $2}'"
     alias linuxbooster="sudo bash /usr/games/linuxbooster/linuxbooster.sh"
      alias mu-uninstall="sudo bash /usr/games/feature-cmd/uninstall.sh"
     alias mu-install="sudo bash /usr/games/feature-cmd/install.sh"
-    f-cmd-update="sudo git pull  /usr/games/feature-cmd/"
+    alias f-cmd-update="sudo git pull  /usr/games/feature-cmd/"
+    alias s="sudo -s"
+    alias listdisk="lsblk"
+    alias temp="watch -n 2 sensors"
+    alias sysinfo="neofetch"
 }
